3036589bbd77f181c38c0e8f00d24f47820be4c6
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-17 15:02:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,19 +15,19 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+  WIN_COMBINATIONS.each {|combo|
+    index_0 = combo[0]
+    index_1 = combo[1]
+    index_2 = combo[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
+      return combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+      return combo
     end
   }
   return false
