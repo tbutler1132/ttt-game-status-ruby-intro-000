8024f4f278215fb8e917b862fff5a0e99720ae28
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-17 14:55:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,9 +45,9 @@ def full?(board)
 end
 
 def draw?(board)
- if !won?(board) && full(board)
-   return true
- else
-   return false
- end
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
 end
