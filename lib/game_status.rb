06fd24706fd1ef29dfee563f0378adb9cb364b72
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-17 15:00:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,3 +61,19 @@ def over?(board)
     return false
   end
 end
+
+def winner(board)
+
+def winner (board)
+    index = []
+    index = won?(board)
+    if index == false
+      return nil
+    else
+      if board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    end
+end
